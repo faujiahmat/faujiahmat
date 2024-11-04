c52193c90917528bc404f07c6883769543301bed
faujiahmat <ahmatfaujimrp@gmail.com>
2024-11-05 01:09:47 +0700

    feat: add-menambahkan desain dan responsive

--- src/main.js
+++ src/main.js
@@ -1,3 +1,17 @@
 import './style.css';
 
+function scrollToSection(linkId, sectionId) {
+  document.getElementById(linkId).addEventListener('click', function (event) {
+    event.preventDefault();
+
+    const section = document.querySelector(sectionId);
+    section.scrollIntoView({ behavior: 'smooth' });
+  });
+}
+
+scrollToSection('about-link', '#about');
+scrollToSection('project-link', '#project');
+scrollToSection('tech-link', '#tech');
+
+
 
