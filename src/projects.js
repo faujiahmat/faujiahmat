213db20087ff688088bd8146c1cd82c9d787c32d
faujiahmat <ahmatfaujimrp@gmail.com>
2025-10-08 00:37:00 +0700

    feat : add zentra

--- src/projects.js
+++ src/projects.js
@@ -1,5 +1,191 @@
 export const projects = [
   {
+    title: 'Zentra | An E-commerce App Microservice',
+    image: './zentra.webp',
+    link: 'https://github.com/faujiahmat/zentra-microservices',
+    centerTitle: true,
+    description: '',
+
+    tabs: [
+      //   {
+      //     name: 'technologies',
+      //     label: 'Technologies',
+      //     content: `
+      //     <div class="grid grid-cols-5 gap-5 px-5 ">
+      //       <div>
+      //         <h3 class="font-bold">
+      //           Language
+      //         </h3>
+      //         <ul class="list-disc ml-5">
+      //           <li>Typescript</li>
+      //         </ul>
+      //       </div>
+      //       <div>
+      //         <h3 class="font-bold">
+      //           Runtime
+      //         </h3>
+      //         <ul class="list-disc ml-5">
+      //           <li>NodeJs</li>
+      //         </ul>
+      //       </div>
+      //       <div>
+      //         <h3 class="font-bold">
+      //           Framework
+      //         </h3>
+      //         <ul class="list-disc ml-5">
+      //           <li>Express</li>
+      //         </ul>
+      //       </div>
+      //       <div>
+      //         <h3 class="font-bold">
+      //           Database
+      //         </h3>
+      //         <ul class="list-disc ml-5">
+      //           <li>MongoDB</li>
+      //           <li>Postgresql</li>
+      //           <li>Redis</li>
+      //         </ul>
+      //       </div>
+      //       <div>
+      //         <h3 class="font-bold">
+      //         API Gateway
+      //         </h3>
+      //         <ul class="list-disc ml-5">
+      //           <li>Kong Gateway</li>
+      //         </ul>
+      //       </div>
+      //       <div>
+      //         <h3 class="font-bold">
+      //         Container Runtime
+      //         </h3>
+      //         <ul class="list-disc ml-5">
+      //           <li>Docker</li>
+      //           <li>Docker Compose</li>
+      //         </ul>
+      //       </div>
+      //       <div>
+      //         <h3 class="font-bold">
+      //         Monitoring
+      //         </h3>
+      //         <ul class="list-disc ml-5">
+      //           <li>Prometheus</li>
+      //           <li>Grafana</li>
+      //           <li>Sentry (Error monitoring)</li>
+      //         </ul>
+      //       </div>
+      //       <div>
+      //         <h3 class="font-bold">
+      //         Message Broker
+      //         </h3>
+      //         <ul class="list-disc ml-5">
+      //           <li>Rabbitmq</li>
+      //         </ul>
+      //       </div>
+      //       <div>
+      //         <h3 class="font-bold">
+      //         Authentication
+      //         </h3>
+      //         <ul class="list-disc ml-5">
+      //           <li>JWT</li>
+      //         </ul>
+      //       </div>
+      //       <div>
+      //         <h3 class="font-bold">
+      //         Orm
+      //         </h3>
+      //         <ul class="list-disc ml-5">
+      //           <li>Prisma</li>
+      //         </ul>
+      //       </div>
+      //     </div>
+      //     `,
+      //   },
+      //   {
+      //     name: 'architecture',
+      //     label: 'Architecture',
+      //     content: `<p class="px-5 mb-5">You can see the architecture of this project here.</p>
+      //     <div class="flex justify-center items-center px-5">
+      //       <a href="https://uploadimage.io/image/MIcroservice-cartonic.SVsdk" target="_blank"><img class="w-40" src="https://uploadimage.io/images/2025/04/18/MIcroservice-cartonic.th.jpg" alt="MIcroservice cartonic" border="0"></a>
+      //     </div>
+      //     `,
+      //   },
+      //   {
+      //     name: 'structure',
+      //     label: 'Structure',
+      //     content: `
+      //       <p class="px-5">
+      //         CARTONIC/</br>
+      //         ├── api-gateway/         # Konfigurasi Kong </br>
+      //         ├── docker/              # Dockerfile dan docker-compose.yml </br>
+      //         ├── docs/                # Dokumentasi API</br>
+      //         ├── scripts/             # Script otomatisasi</br>
+      //         ├── services/            # Semua service</br>
+      //         ├── .gitignore</br>
+      //         └── README.md
+      //       </p>
+      //     `,
+      //   },
+      //   {
+      //     name: 'api',
+      //     label: 'API Documentation',
+      //     content: `<p class="px-5">You can see api documentaion <a href="https://github.com/faujiahmat/Cartonic-Microservices-E-commerce-App/blob/main/docs/api.md" target="_blank" class="font-bold text-cyan-200 hover:text-cyan-600">here</a></p>`,
+      //   },
+      //   {
+      //     name: 'feature',
+      //     label: 'Feature',
+      //     content: `
+      //       <div class="grid  grid-cols-6  gap-1 auto-rows-auto">
+      //         <div class="bg-cyan-900 rounded-xl p-1 text-white h-fit shadow-lg">
+      //           <h3 class="font-bold">Order Service</h3>
+      //           <p class="text-xs">Simulasi pemesanan produk.</p>
+      //         </div>
+      //         <div class="bg-cyan-900 rounded-xl p-1 text-white h-fit shadow-lg col-span-2">
+      //           <h3 class="font-bold">Auth Authentication</h3>
+      //           <p class="text-xs">Register, login, logout dan refresh token dengan validasi JWT dan hashed password (bcrypt).</p>
+      //         </div>
+      //         <div class="bg-cyan-900 rounded-xl p-1 text-white h-fit shadow-lg">
+      //           <h3 class="font-bold">User Management</h3>
+      //           <p class="text-xs">Memanage profile user.</p>
+      //         </div>
+      //         <div class="bg-cyan-900 rounded-xl p-1 text-white h-fit shadow-lg col-span-2">
+      //           <h3 class="font-bold">Monitoring</h3>
+      //           <p class="text-xs">Prometheus memonitor seluruh service, ditampilkan di Grafana dan error monitoring dengan Sentry.</p>
+      //         </div>
+      //         <div class="bg-cyan-900 rounded-xl p-1 text-white h-fit shadow-lg">
+      //           <h3 class="font-bold">Product Management</h3>
+      //           <p class="text-xs">Admin bisa menambahkan, mengedit, dan menghapus produk.</p>
+      //         </div>
+      //         <div class="bg-cyan-900 rounded-xl p-1 text-white h-fit shadow-lg">
+      //           <h3 class="font-bold">Category System</h3>
+      //           <p class="text-xs">Produk bisa dikelompokkan ke dalam berbagai kategori.</p>
+      //         </div>
+      //         <div class="bg-cyan-900 rounded-xl p-1 text-white h-fit shadow-lg">
+      //           <h3 class="font-bold">Role-based Access</h3>
+      //           <p class="text-xs">Role user dan admin dipisahkan. Admin punya kontrol penuh.</p>
+      //         </div>
+      //         <div class="bg-cyan-900 rounded-xl p-1 text-white h-fit shadow-lg">
+      //         <h3 class="font-bold">Payment Service</h3>
+      //         <p class="text-xs">Melakukan simulasi pembayaran yang berasal dari payment-gateway.</p>
+      //         </div>
+      //         <div class="bg-cyan-900 rounded-xl p-1 text-white h-fit shadow-lg ">
+      //           <h3 class="font-bold col-span-2">Kong Gateway</h3>
+      //           <p class="text-xs">Sebagai pintu utama akses tiap service, validasi token JWT dan rate limiting.</p>
+      //         </div>
+      //         <div class="bg-cyan-900 rounded-xl p-1 text-white h-fit shadow-lg">
+      //           <h3 class="font-bold">Search & Filter</h3>
+      //           <p class="text-xs">Cari produk berdasarkan nama, kategori, harga, rating dan lain-lain.</p>
+      //         </div>
+      //       </div>
+      //     `,
+      //   },
+      //   {
+      //     name: 'readme',
+      //     label: 'Readme',
+      //     content: `<p class="px-5">You can see detail of this project <a href="https://github.com/faujiahmat/Cartonic-Microservices-E-commerce-App/blob/main/README.md" class="text-cyan-200 hover:text-cyan-600 font-bold" target="_blank" >here</a></p>`,
+      //   },
+    ],
+  },
+  {
     title: 'Cartonic Microservices E-commerce App',
     image: './e-commerce.webp',
     link: 'https://github.com/faujiahmat/Cartonic-Microservices-E-commerce-App',
